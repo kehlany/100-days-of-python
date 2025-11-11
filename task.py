853cb2d4923d848ad999709cddd0654d18d935a8
kehlany <120030843+kehlany@users.noreply.github.com>
2025-11-11 21:06:50 +0100

    Add files via upload

--- task.py
+++ task.py
@@ -1,22 +1,46 @@
 import random
-letters =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-total = []
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-for nr_letters in range(1, nr_letters + 1):
-    total.append(random.choice(letters))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-for nr_symbols in range(1, nr_symbols + 1):
-    total.append(random.choice(symbols))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-for nr_numbers in range(1, nr_numbers + 1):
-    total.append(random.choice(numbers))
-print(total)
-random.shuffle(total)
-print(total)
-password = ''
-for word in total:
-    password += word
-print(password)
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+game = random.choice([rock, paper, scissors])
+user_input = int (input('what do you choose? type 0 for rock, 1 for paper or 2 for scissors'))
+if user_input == 0:
+    print(rock)
+elif user_input == 1:
+    print(paper)
+else:
+    print(scissors)
+print(game,user_input)
+if game == 'paper' and user_input == 'rock':
+    print('you lose')
+elif game == 'scissors' and user_input == 'paper':
+    print('you lose')
+elif game == 'rock' and user_input == 'scissors':
+    print('you lose')
+elif game == user_input:
+    print('its a draw')
+else:
+    print('you win')
